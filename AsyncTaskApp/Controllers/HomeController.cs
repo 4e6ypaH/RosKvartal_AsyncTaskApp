4f188b0ff5e105f4aed77ed83ffdc0e10f0ef79f
Maxim Troshkov <maxitm_troshkov@mail.ru>
2018-11-27 22:47:15 +0300

    lesson 15

--- AsyncTaskApp/Controllers/HomeController.cs
+++ AsyncTaskApp/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using AsyncTaskApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,22 @@ namespace AsyncTaskApp.Controllers
 {
     public class HomeController : Controller
     {
+        BookContext db = new BookContext();
         public ActionResult Index()
         {
+            IEnumerable<Book> books = db.Books.ToList();
+            ViewBag.Books = books;
             return View();
+        }
+
+        //IIS - пул потоков 2000-5000
+
+        //асинхронный метод
+        public async Task<ActionResult> BookList()
+        {
+            IEnumerable<Book> books = await db.Books.ToListAsync();
+            ViewBag.Books = books;
+            return View("Index");
         }
 
         public ActionResult About()
